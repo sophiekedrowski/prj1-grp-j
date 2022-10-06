ea9c14c8aaa3e811a6b1c8aa33959867110973d8
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2022-10-06 18:24:54 -0500

    Any additional work that I have done

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,32 @@
+//project requirements
+// Use a CSS framework other than Bootstrap.
+// Be deployed to GitHub Pages.
+// Be interactive (in other words, accept and respond to user input).
+// Use at least two server-side APIsLinks to an external site..
+// Use modals instead of alerts, confirms, or prompts.
+// Use client-side storage to store persistent data.
+// Be responsive.
+// Have a polished UI.
+// Have a clean repository that meets quality coding standards (file structure, naming conventions, best practices for class/id naming conventions, indentation, quality comments, and so on).
+// Have a quality README (including a unique name, description, technologies used, screenshot, and link to the deployed application).
 
+
+// fetch('https://maps.googleapis.com/maps/api/staticmap?center')
+// .then((response) => response.json())
+// .then((data) => console.log(data));
+
+ var NPSApiKey = "wjZQ9PWzvuBiFbcfbbU3vKcpyOCQ7Hl9n8bTOQQL"
+ var endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
+
+
+// fetch(endpoint, {
+//   headers: {Authentication: 'wjZQ9PWzvuBiFbcfbbU3vKcpyOCQ7Hl9n8bTOQQL'}
+// })
+//    .then(resp => resp.json())
+//    .then(json => console.log(json))
+
+
+fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=wjZQ9PWzvuBiFbcfbbU3vKcpyOCQ7Hl9n8bTOQQL'
+)
+.then((response) => response.json())
+.then((data) => console.log(data));
